1189fb71e6a2761ba888ae3ca5b9c2142de27c5d
Azabu-bound <tylerjbailey95@gmail.com>
2024-07-27 16:47:25 -0400

    Setting up http

--- main.zig
+++ main.zig
@@ -1,19 +1,12 @@
 const std = @import("std");
+const http = std.http;
+const json = std.json;
 
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
 
-pub fn main() void {
-	const usdAmount = 100.0;
-
-	const euroRate = 0.93;
-	const gbpRate = 0.82;
-	const jpyRate = 134.25;
-
-	const usdToEuro = usdAmount * euroRate;
-	const usdToGBP = usdAmount * gbpRate;
-	const usdToJPY = usdAmount * jpyRate;
-
-	std.debug.print("USD {d:.2} is equivalent to EUR {d:.2}\n", .{usdAmount, usdToEuro});
-	std.debug.print("USD {d:.2} is equivalent to GBP {d:.2}\n", .{usdAmount, usdToGBP});
-	std.debug.print("USD {d:.2} is equivalent to JPY {d:.2}\n", .{usdAmount, usdToJPY});
-
+    var client = http.Client{ .allocator = allocator };
+    defer client.deinit();
 }
